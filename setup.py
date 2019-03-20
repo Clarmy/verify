920d35552b52730f9764cc93484b1198c8ad09bc
Clarmy Lee <clarmylee92510@gmail.com>
2019-03-20 19:21:22 +0800

    添加setup文件

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='verify',
+    version='0.0.1.dev1',
+    description='A package to perform verifying NWP',
+    url='https://github.com/Clarmy/verify',
+    author='Clarmy Lee',
+    author_email='liwentao@mail.iap.ac.cn',
+    license='GPL-3.0',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development :: Quality Assurance',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='verify NWP',
+    packages=['verify'],
+)
